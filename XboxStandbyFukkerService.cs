bfececb1a7da8e584436b8afe6855c715a6ab017
Maximilian-PC <maximilianklassen@outlook.com>
2021-06-27 15:47:08 +0200

    Add method to check if headset is connected

--- XboxStandbyFukkerService.cs
+++ XboxStandbyFukkerService.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,10 @@ namespace XboxStandbyFukker
         private readonly EventLog Log = new EventLog();
         private const string EVENT_SOURCE = "Xbox-Standby-Fukker";
         private const string EVENT_LOG = "Application";
+        private static readonly string[] XBOX_HWIDS = new[]
+        {
+            @"USB\VID_045E&PID_0B17&IGA_00"
+        };
 
         public XboxStandbyFukkerService()
         {
@@ -28,7 +33,30 @@ namespace XboxStandbyFukker
             Log.Source = EVENT_SOURCE;
             Log.Log = EVENT_LOG;
 
+            bool test = IsHeadsetConnected();
+
             InitializeComponent();
+        }
+
+        private bool IsHeadsetConnected()
+        {
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_SoundDevice");
+            var collection = searcher.Get();
+
+            foreach (var obj in collection)
+            {
+                foreach (var prop in obj.Properties)
+                {
+                    if (prop?.Value is not null
+                        && prop.Value is string value
+                        && XBOX_HWIDS.Any(x => value.Contains(x)))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
 
         protected override void OnStart(string[] args)
