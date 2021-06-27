6699295ed01555b062dfa1f227efc98c6239c88d
Maximilian-PC <maximilianklassen@outlook.com>
2021-06-27 15:20:42 +0200

    Finish log4net implementation

--- XboxStandbyFukkerService.cs
+++ XboxStandbyFukkerService.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,19 @@ namespace XboxStandbyFukker
 {
     public partial class XboxStandbyFukkerService : ServiceBase
     {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         public XboxStandbyFukkerService()
         {
-            InitializeComponent();
+            Log.Info("Initialize Xbox Standby Fukker Service...");
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
         }
 
         protected override void OnStart(string[] args)
