02909dc86507536d1f64e13e2dcc7c7d32ded718
Maximilian-PC <maximilianklassen@outlook.com>
2021-06-27 15:30:27 +0200

    Add logging

--- XboxStandbyFukkerService.cs
+++ XboxStandbyFukkerService.cs
@@ -12,8 +12,23 @@ namespace XboxStandbyFukker
 {
     public partial class XboxStandbyFukkerService : ServiceBase
     {
+        private readonly EventLog Log = new EventLog();
+        private const string EVENT_SOURCE = "Xbox-Standby-Fukker";
+        private const string EVENT_LOG = "Application";
+
         public XboxStandbyFukkerService()
         {
+            AutoLog = false;
+
+            if (!EventLog.SourceExists(EVENT_SOURCE))
+            {
+                EventLog.CreateEventSource(EVENT_SOURCE, EVENT_LOG);
+            }
+
+            Log.Source = EVENT_SOURCE;
+            Log.Log = EVENT_LOG;
+            Log.WriteEntry("Initialize Xbox Standby Fukker", EventLogEntryType.Information);
+
             InitializeComponent();
         }
 
