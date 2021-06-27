efd191cec18090d8175472d632b9090c4fe3b9f0
Maximilian-PC <maximilianklassen@outlook.com>
2021-06-27 15:12:55 +0200

    Revert "Finish DependencyInjection"
    This reverts commit c0e4733b4144566a73dcd21ebeafefc125203b91, reversing
    changes made to ca087e71ae520ad3f07286dc734813eeaa1c4c8d.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿using Autofac;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace XboxStandbyFukker
 {
@@ -7,16 +11,12 @@ namespace XboxStandbyFukker
     {
         static void Main()
         {
-            var builder = new ContainerBuilder();
-
-            // Register windows service
-            builder.RegisterType<XboxStandbyFukkerService>().AsSelf().InstancePerLifetimeScope();
-
-            // Register dependencies
-            //builder.RegisterType<ClassType>().InstancePerLifetimeScope();
-
-            // Start service
-            ServiceBase.Run(builder.Build().Resolve<XboxStandbyFukkerService>());
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new XboxStandbyFukkerService()
+            };
+            ServiceBase.Run(ServicesToRun);
         }
     }
 }
